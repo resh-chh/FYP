43780b5c0bd197264be1673cd204a6d008956115
Vaibhav Goyal <Vaibhav Goyal>
2017-04-24 19:19:04 +0530

    Responsiveness added to Donation Page

--- html/Donation.php
+++ html/Donation.php
@@ -68,134 +68,17 @@ nav li a:hover {background-color: #555;}
 {
   border-radius: 5px;
   border-color: red;
+  font-size: 20px;
+  margin-left: 10px;
+  padding-left: 15px;
+  padding : 5px;
   
 
 }
+
+
 ::-webkit-input-placeholder { color:darkgrey; }
 
- #one :focus ~ .floating-label1,
-input:not(:focus):valid ~ .floating-label1{
-  left: 705px;
-  top: 325px;
-  font-size: 14px;
-  opacity: 1;
-}
-
-.inputText {
-  padding-left: 10px;
-  width: 250px;
-  height: 35px;
-  font-size: 18px;
-}
-
-.floating-label1 {
-  position: absolute;
-  pointer-events: none;
-  left: 630px;
-  top: 290px;
-  font-size: 18px;
-  transition: 0.2s ease all;
-  opacity: 0.5;
-}
-
-#two :focus ~ .floating-label2,
-input:not(:focus):valid ~ .floating-label2{
-  left: 705px;
-  top: 390px;
-  font-size: 14px;
-  opacity: 1;
-}
-
-
-.floating-label2 {
-  position: absolute;
-  pointer-events: none;
-  left: 630px;
-  top: 355px;
-  font-size: 18px;
-  transition: 0.2s ease all;
-  opacity: 0.5;
-}
-
-#three :focus ~ .floating-label3,
-input:not(:focus):valid ~ .floating-label3{
-  left: 705px;
-  top: 460px;
-  font-size: 14px;
-  opacity: 1;
-}
-
-
-
-.floating-label3  {
-  position: absolute;
-  pointer-events: none;
-  left: 630px;
-  top: 420px;
-  font-size: 18px;
-  transition: 0.2s ease all;
-  opacity: 0.5;
-}
-
-#four :focus ~ .floating-label4,
-input:not(:focus):valid ~ .floating-label4{
-  left: 705px;
-  top: 525px;
-  font-size: 14px;
-  opacity: 1;
-}
-
-
-
-.floating-label4  {
-  position: absolute;
-  pointer-events: none;
-  left: 630px;
-  top: 490px;
-  font-size: 18px;
-  transition: 0.2s ease all;
-  opacity: 0.5;
-}
-
-#five :focus ~ .floating-label5,
-input:not(:focus):valid ~ .floating-label5{
-  left: 705px;
-  top: 590px;
-  font-size: 14px;
-  opacity: 1;
-}
-
-
-
-.floating-label5   {
-  position: absolute;
-  pointer-events: none;
-  left: 630px;
-  top: 555px;
-  font-size: 18px;
-  transition: 0.2s ease all;
-   opacity: 0.5;
-}
-
-#six :focus ~ .floating-label6,
-input:not(:focus):valid ~ .floating-label6{
-  left: 705px;
-  top: 655px;
-  font-size: 14px;
-  opacity: 1;
-}
-
-
-
-.floating-label6   {
-  position: absolute;
-  pointer-events: none;
-  left: 630px;
-  top: 615px;
-  font-size: 18px;
-  transition: 0.2s ease all;
-   opacity: 0.5;
-}
 
 li:last-child{
   float: right;
@@ -217,6 +100,35 @@ li:last-child{
       font-family: 'Merienda One', cursive;
       font-size: 15px;
     }
+
+ @media screen and (max-width: 800px)
+ {
+     #box
+{
+  border-radius: 4px;
+  border-color: red;
+  font-size: 15px;
+  margin-left: 7px;
+  padding-left: 15px;
+  padding : 6px;
+  width: 150px;
+
+  
+
+} 
+
+ #donation_form
+  {
+    border-height: 100px;
+    border-width: 100px;
+    border-style: solid 5px;
+    border-color: black;
+    font-family: sans-serif;
+    font-size: 15px;
+  }
+ }
+
+
 
 	</style>
 </head>
@@ -243,46 +155,45 @@ li:last-child{
 <h1 id="border_box" align="center">Donate for the Pets</h1>
    <form id="donation_form" action="../php/donate.php" method="POST" align="center">
      
-        <div id="one">
-         Name <input id="box" name="name" type="text" class="inputText" required/>
-         <span class="floating-label1">Enter your name</span>
+     
+        <div >
+         Name <input id="box" name="name" type="text" class="inputText" required placeholder="Enter your name" />
+        
       </div><br>
-        <div id="two">
-         Email-Id <input id="box" name="email" type="text" class="inputText" required/>
-         <span class="floating-label2">Enter your Email-Id</span>
+        <div >
+         Email-Id <input id="box" name="email" type="text" class="inputText" required placeholder="Enter your Email-Id" />
+        
       </div><br>
          
-         <div id="three">
-         Contact <input id="box" name="phone" type="text" class="inputText" required/>
-         <span class="floating-label3">Enter your Contact No</span>
+         <div >
+         Contact <input id="box" name="phone" type="text" class="inputText" required placeholder="Enter your Contact Number" />
+         
       </div><br>
 
-      <div id="four">
-         Address <input id="box" name="add" type="text" class="inputText" required/>
-         <span class="floating-label4">Enter your Address</span>
-      </div><br>
-
-          <div id="five">
-         Donation <input id="box" name="amount" type="text" class="inputText" required/>
-         <span class="floating-label5">Enter the amount</span>
-      </div><br>
-
-       <div id="six">
-         DOB <input id="box" name="dob" align="center" type="date" class="inputText" required/>
+      <div >
+         Address <input id="box" name="add" type="text" class="inputText" required placeholder="Enter your Address" />
         
       </div><br>
 
-      
+          <div>
+         Donation <input id="box" name="amount" type="text" class="inputText" required placeholder="Enter the amount" />
+         
+      </div><br>
+
+       <div>
+         DOB <input id="box" name="dob" align="center" type="date" class="inputText" required />
+        
+      </div><br>
 
       <input  type="submit" data-inline="true" value="Donate Now" id="sub"><br><br>
-
+      </div>
 
 
 
    </form>
 </div>
 <div class="bot">
-  <?php include 'html/footer.php' ;?>
+  <?php include '../html/footer.php' ;?>
 </div>
 </body>
 </html>
